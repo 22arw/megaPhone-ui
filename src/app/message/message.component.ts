aae090d5e7f551fbf4a96b33e36abff6e22bd0f0
Kevin Falting <kevinfalting@gmail.com>
2018-11-25 10:05:48 -0600

    a

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -47,10 +47,18 @@ export class MessageComponent implements OnInit, OnChanges {
       'Let everyone know their impact. 🙌',
       'It is always a good idea to share good news. 👍',
       'What a week! 💪',
-      'Catch me later, I\'ll buy you a beer. 🍻'
+      // tslint:disable-next-line:quotemark
+      "Catch me later, I'll buy you a beer. 🍻",
+      // tslint:disable-next-line:quotemark
+      "If the pilot's good, see, I mean if he's reeeally sharp, he'll send a message right here ✈",
+      // tslint:disable-next-line:quotemark
+      "I shouldn't tell you this, Mandrake, but you're a good officer and you've a right to know. It looks like we're in a shooting war.",
+      // tslint:disable-next-line:max-line-length quotemark
+      "Okay, I'm going to get your money for you, but if you don't get the president of the United States on that phone, you know what's going to happen to you? What!? You're gonna have to answer to the Coca-Cola company. ☢"
     ];
 
-    this.placeholder = placeholders[Math.floor(Math.random() * placeholders.length)];
+    this.placeholder =
+      placeholders[Math.floor(Math.random() * placeholders.length)];
   }
 
   outputMessage(e: Event) {
