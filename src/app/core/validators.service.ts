6b1fdc084405fed03ff3669483f037482f6af801
Kevin Falting <kevinfalting@gmail.com>
2018-12-14 13:37:04 -0600

    update emails to not allow .mil and .gov

--- src/app/core/validators.service.ts
+++ src/app/core/validators.service.ts
@@ -19,10 +19,15 @@ export class ValidatorService {
       return false;
     }
 
-    const validDomainsArray = ['mil', 'gov', 'com'];
+    const validDomainsArray = ['mil', 'gov'];
     const validDomains = validDomainsArray.join('|');
+
+    // Do not allow Valid Domains
     // tslint:disable-next-line:max-line-length
-    const regex = `[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(${validDomains})$`;
+    const regex = `[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?!(${validDomains})).*$`;
+
+    // tslint:disable-next-line:max-line-length
+    // const regex = `[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(${validDomains})$`;
     const pattern = new RegExp(regex, 'gm');
 
     return pattern.test(email);
